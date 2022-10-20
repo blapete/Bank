fe5e3b7934228b8c1f8219f45385bc68ce4f5b8e
Peebs <blank-p@outlook.com>
2022-10-20 00:24:57 -0500

    validate amount

--- account.py
+++ account.py
@@ -7,10 +7,17 @@ class Account():
 
     def __init__(self, name, balance, password):
         self.name = name
-        self.balance = 
+        self.balance = self.validateAmount(balance)
         self.password = password
 
     def validateAmount(self, amount):
+        try:
+            amount = int(amount)
+        except ValueError:
+            raise AbortTransaction('Amount must be an integer')
+        if amount <= 0:
+            raise AbortTransaction('Amount must be positive')
+        return amount
 
     def checkPasswordMatch(self, password):
 
