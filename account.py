00b65f8ee9e808f93e0b841774f346b7f11dca07
Peebs <blank-p@outlook.com>
2022-10-20 00:24:32 -0500

    account instance variables and methods

--- account.py
+++ account.py
@@ -3,3 +3,22 @@ class AbortTransaction(Exception):
     '''abort transaction'''
     pass
 
+class Account():
+
+    def __init__(self, name, balance, password):
+        self.name = name
+        self.balance = 
+        self.password = password
+
+    def validateAmount(self, amount):
+
+    def checkPasswordMatch(self, password):
+
+    def deposit(self, amountToDeposit):
+
+    def getBalance(self):
+
+    def withdraw(self, amountToWithdraw):
+
+    # Testing
+    def show(self):
